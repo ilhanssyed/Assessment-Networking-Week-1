1946366bdb5ccdcab465d36fad41165539cfe41e
ilhanssyed <ilhanssyed@hotmail.com>
2026-05-01 17:22:23 +0100

    cache functionality attemp

--- postcode_functions.py
+++ postcode_functions.py
@@ -11,7 +11,7 @@ def load_cache() -> dict:
     """Loads the cache from a file and converts it from JSON to a dictionary."""
     # This function is used in Task 3, you can ignore it for now.
     try:
-        with open(CACHE_FILE, "r"):
+        with open(CACHE_FILE, "r") as f:
             return json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
@@ -22,8 +22,8 @@ def load_cache() -> dict:
 def save_cache(cache: dict):
     """Saves the cache to a file as JSON"""
     # This function is used in Task 3, you can ignore it for now.
-    with open(CACHE_FILE, "w"):
-        json.dump(cache, f)
+    with open(CACHE_FILE, "w") as f:
+        json.dump(cache, f, indent=3)
     ...
 
 
@@ -33,7 +33,14 @@ def validate_postcode(postcode: str) -> bool:
         raise TypeError("Function expects a string.")
 
     formatted_postcode = postcode.upper().strip()
+
+    cache = load_cache()
+
+    if postcode in cache and "valid" in cache[postcode]:
+        return cache[postcode]["valid"]
+
     url = f"https://api.postcodes.io/postcodes/{formatted_postcode}/validate"
+
     response = req.get(url)
     if response.status_code >= 500:
         raise req.RequestException(f"Unable to access API.")
@@ -41,6 +48,9 @@ def validate_postcode(postcode: str) -> bool:
     response.raise_for_status()
 
     data = response.json()
+    cache.setdefault(postcode, {})
+    cache[postcode]["valid"] = result
+    save_cache(cache)
 
     return data.get("result", False)
 
@@ -77,6 +87,11 @@ def get_postcode_completions(postcode_start: str) -> list[str]:
 
     response = req.get(url)
 
+    cache = load_cache()
+
+    if postcode_start in cache and "completions" in cache[postcode_start]:
+        return cache[postcode_start]["completions"]
+
     if response.status_code >= 500:
         raise req.RequestException("Unable to access API.")
 
@@ -85,6 +100,10 @@ def get_postcode_completions(postcode_start: str) -> list[str]:
     data = response.json()
 
     value = data.get("result", [])
+
+    cache.setdefault(postcode_start, {})
+    cache[postcode_start]["valid"] = result
+    save_cache(cache)
 
     return value[:5]
 
