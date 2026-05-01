bef708aab6f6e79edafc25368d4ef7ca5c58decc
ilhanssyed <ilhanssyed@hotmail.com>
2026-05-01 15:54:40 +0100

    starting cli

--- postcode_cli.py
+++ postcode_cli.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
         description="Takes in a postcode and returns the information",
         epilog="Contact S1gma L8bs for help")
 
-    parser.add_argument("--mode", "-m", help="Enter validate or complete")
+    parser.add_argument("--mode", "-m", required=True,
+                        help="Enter validate or complete")
     parser.add_argument("postcode", help="Enter a postcode")
 
     args = parser.parse_args()
+
+    postcode = args.postcode.upper().strip()
